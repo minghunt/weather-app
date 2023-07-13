e2b6c15c1e9a001d7f49796d7327309083792f9a
minghunt <114232628+minghunt@users.noreply.github.com>
2023-07-14 06:00:37 +0700

    Create project

---
+++ src/app.js
@@ -0,0 +1,84 @@
+import express from 'express'
+import hbs from 'hbs'
+import path from 'path';
+import { title } from 'process';
+import { fileURLToPath } from 'url';
+import * as untils from './untils/untils.js'
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const app = express()
+
+const publicDirectoryPath = path.join(__dirname,'../public')
+const viewsPath = path.join(__dirname,'../templates/views')
+const partialsPath = path.join(__dirname,'../templates/partials')
+
+app.set('view engine','hbs')
+app.set('views',viewsPath)
+hbs.registerPartials(partialsPath)
+
+app.use(express.static(publicDirectoryPath))
+app.get('',(req,res)=>{
+    res.render('index',{
+        title:'Weather',
+        name:'Minh Hung'
+    })
+})
+app.get('/about',(req,res)=>{
+    res.render('about',{
+        title:'About me',
+        name:'Minh Hung'
+    }) 
+})
+app.get('/help',(req,res)=>{
+    res.render('help',{
+        helpText:'Help text.',
+        title:'Help',
+        name:'Minh Hung'
+    })
+})   
+app.get(`/weather`, (req, res) => {
+    if (!req.query.address)
+    {
+        return res.send({
+            error:'You must provide an address'
+        })
+    }
+ 
+    untils.geocode(req.query.address, (error, { latitude, longtitude, location }={}) => {
+        if (error)
+            return res.send({error})
+        else {
+            untils.forecast(latitude, longtitude, (error, forecastData) => {
+                if (error)
+                    return console.log(error)
+                console.log(location)
+                console.log(forecastData)
+                res.send({
+                    forecast:forecastData,
+                    location:location,
+                    address:req.query.address
+                })
+            })
+        }
+    })
+
+    
+}) 
+app.get(`/help/*`, (req, res) => {
+    res.render('404',{
+        errorMessage:'Help article not found',
+        title:'404',
+        name:'Minh Hung' 
+    }) 
+})  
+app.get(`*`, (req, res) => {
+    res.render('404',{
+        errorMessage:'Page not found',
+        title:'404',
+        name:'Minh Hung'
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000.')
+})
---
+++ src/untils/untils.js
@@ -0,0 +1,37 @@
+import request from "request";
+
+const geocode = (address, callback) => {
+    const url = 'http://nominatim.openstreetmap.org/search?format=json&limit=3&q=' + address
+    request({ url: url }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to location service!', undefined)
+        } else if (body === '[]') {
+            callback('Unable to find location! Try another search.', undefined)
+        }
+        else {
+            const data = JSON.parse(body)
+            callback(undefined, {
+                latitude: data[0].lat,
+                longtitude: data[0].lon,
+                location: data[0].display_name
+            })
+        }
+    })
+}
+
+const forecast = (lat, lon, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=87853003a8a4192b8664b5ccd981fc78&query=' + lat + ',' + lon
+    request({ url: url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined)
+        } else if (body.error) {
+            callback('Unable to find location!', undefined)
+        }
+        else {
+            callback(undefined, body.current.weather_descriptions + '. It is currently ' + body.current.temperature + ' degress out. The wind speed is '
+            + body.current.wind_speed+'km/h')
+        }
+    })
+} 
+
+export { geocode, forecast }
