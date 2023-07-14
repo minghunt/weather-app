d762fc36ba349c87ce59174b1b78c89b20582480
minghunt <114232628+minghunt@users.noreply.github.com>
2023-07-14 09:02:43 +0700

    Add vercel.json

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express'
 import hbs from 'hbs'
 import path from 'path';
 import { fileURLToPath } from 'url';
-import * as untils from './untils/untils.js'
+import * as untils from './src/untils/untils.js'
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const app = express()
