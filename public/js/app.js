813d673272e2134e33590bf177a1bd6f7bd463fb
minghunt <114232628+minghunt@users.noreply.github.com>
2023-07-14 11:21:51 +0700

    Add vercel.json

--- public/js/app.js
+++ public/js/app.js
@@ -9,7 +9,12 @@ weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
     message1.textContent='Loading...'
     message2.textContent=''
-    fetch('/weather?address='+search.value)
+    fetch('/weather?address='+search.value, {
+        mode: 'cors',
+        headers: {
+          'Access-Control-Allow-Origin':'*'
+        }
+      })
         .then((response) => {
             response.json().then((data) => {
                 if (data.error)
